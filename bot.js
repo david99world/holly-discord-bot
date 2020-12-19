aed74398e42a2a1c1e2260d4678503dfae60ac9d
David <david99world80@gmail.com>
2020-12-19 18:09:37 +0000

    Removed the Google secret stuff, started metacritic search and added not found error

--- bot.js
+++ bot.js
@@ -6,11 +6,10 @@
 const Discord = require('discord.js');
 const hltb = require('howlongtobeat');
 const commands = require('./commands.js');
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const secretClient = new SecretManagerServiceClient();
-const name = 'projects/768903241973/secrets/discordHollyAuth/versions/latest';
 
+const metacritic = require('metacritic-scraper')
 const holly = 'Holly-game-bot';
+const auth = require('./token/auth.json');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -26,32 +25,49 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
   if (message.author.username != holly && message.content.startsWith(commands.getHltbCommand())) {
-    if(message.guild.available) {
+    if (message.guild.available) {
       console.log("Guild name " + message.guild.name);
-  }
+    }
     writeHltbMessage(message);
   }
+
+  if (message.author.username != holly && message.content.startsWith(commands.getMetaCriticCommand())) {
+    if (message.guild.available) {
+      console.log("Guild name " + message.guild.name);
+    }
+    writeMetaCriticMessage(message);
+  }
 });
+
+async function writeMetaCriticMessage(message) {
+ 
+}
 
 async function writeHltbMessage(message) {
   let gameId = '';
   let hltbService = new hltb.HowLongToBeatService();
-  await hltbService.search(message.content.slice(5)).then(function (response) {
-    gameId = response[0].id;
-  });
-  await hltbService.detail(gameId).then(result => {
-    var output = result.name + " main story is " + result.gameplayMain + 
-    "hrs, extras " + result.gameplayMainExtra + "hrs and completion " + result.gameplayCompletionist + "hrs";
-    console.log(output);
-    console.log(result.name + " asked for");
-    message.channel.send(output);
-  });
+  try {
+    await hltbService.search(message.content.slice(5)).then(function (response) {
+      if (response[0] != null) {
+        gameId = response[0].id;
+      } else {
+        failureMessage = "Sorry no results for" + message.content.slice(5);
+        console.log(failureMessage);
+        message.channel.send(failureMessage);
+      }
+    });
+    if (gameId != null) {
+      await hltbService.detail(gameId).then(result => {
+        var output = result.name + " main story is " + result.gameplayMain +
+          "hrs, extras " + result.gameplayMainExtra + "hrs and completion " + result.gameplayCompletionist + "hrs";
+        console.log(output);
+        console.log("REQUEST :: " + result.name);
+        message.channel.send(output);
+      });
+    }
+  } catch (err) {
+    console.log("Error calling hltb site :: \n " + err)
+  }
 }
-async function login() {
-  const [version] = await secretClient.accessSecretVersion({
-    name: name,
-  });
 
-   client.login(version.payload.data.toString('utf8'));
-}
-login();
+client.login(auth.token);
--- commands.js
+++ commands.js
@@ -1,6 +1,11 @@
 const hltbCommand = '!hltb'
+const metacriticCommand = '!meta'
 
 exports.getHltbCommand = function() {
     return hltbCommand;
+}
+
+exports.getMetaCriticCommand = function() {
+    return metacriticCommand;
 }
 
